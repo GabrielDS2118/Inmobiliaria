d45bc33519e32553b0e016061397832d1b99090b
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2022-10-22 15:12:31 -0500

    Filtrando todo menos por location

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -4,12 +4,100 @@ import { useRouter } from 'next/router';
 import { MdCancel } from 'react-icons/md';
 import Image from 'next/image';
 
-const SearchFilters = () => {
-    return (
-        <div>
-            SearchFilters
-        </div>
-    );
-}
+import { filterData, getFilterValues } from '../utils/filterData';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+import noresult from '../assets/images/noresult.svg';
 
-export default SearchFilters;
+export default function SearchFilters() {
+  const [filters] = useState(filterData);
+  const router = useRouter();
+
+  // Los filter values es la selección de cada filtro
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const {query} = router;
+
+    //Mostrar los valores de los filtros en la url
+    //Automaticamente se buscan los aptos que cumplen con las condiciones
+    const values = getFilterValues(filterValues);
+    values.forEach((item) => {
+      query[item.name] = item.value
+    })
+
+    router.push({pathname: path, query})
+  };
+
+
+  return (
+    <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
+    
+    {/* Se va a recorrer cada uno de los filtros y sus opciones */}
+      {filters?.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select onChange={(e) => searchProperties({ [filter.queryName]: e.target.value })} placeholder={filter.placeholder} w='fit-content' p='2' >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+      
+      {/* <Flex flexDir='column'>
+        <Button onClick={() => setShowLocations(!showLocations)} border='1px' borderColor='gray.200' marginTop='2' >
+          Search Location
+        </Button>
+        {showLocations && (
+          <Flex flexDir='column' pos='relative' paddingTop='2'>
+            <Input
+              placeholder='Type Here'
+              value={searchTerm}
+              w='300px'
+              focusBorderColor='gray.300'
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            {searchTerm !== '' && (
+              <Icon
+                as={MdCancel}
+                pos='absolute'
+                cursor='pointer'
+                right='5'
+                top='5'
+                zIndex='100'
+                onClick={() => setSearchTerm('')}
+              />
+            )}
+            {loading && <Spinner margin='auto' marginTop='3' />}
+            {showLocations && (
+              <Box height='300px' overflow='auto'>
+                {locationData?.map((location) => (
+                  <Box
+                    key={location.id}
+                    onClick={() => {
+                      searchProperties({ locationExternalIDs: location.externalID });
+                      setShowLocations(false);
+                      setSearchTerm(location.name);
+                    }}
+                  >
+                    <Text cursor='pointer' bg='gray.200' p='2' borderBottom='1px' borderColor='gray.100' >
+                      {location.name}
+                    </Text>
+                  </Box>
+                ))}
+                {!loading && !locationData?.length && (
+                  <Flex justifyContent='center' alignItems='center' flexDir='column' marginTop='5' marginBottom='5' >
+                    <Image src={noresult} />
+                    <Text fontSize='xl' marginTop='3'>
+                      Waiting to search!
+                    </Text>
+                  </Flex>
+                )}
+              </Box>
+            )}
+          </Flex>
+        )}
+      </Flex> */}
+    </Flex>
+  );
+}
